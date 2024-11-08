01d03144d5d424f56b1d8d3c4c8248ee0c6b2c86
amrou <amrou.mounsif@marconirovereto.it>
2024-11-08 09:34:23 +0100

    lezione del 8/11 fine lezione

--- script/http-request.js
+++ script/http-request.js
@@ -1,12 +1,42 @@
 function getAereoport(){
     //http://google.com inditizzo assoluto
     fetch('./data/aereoporti.json') //indirizzo relativo
-    .then(Response => {
-        console.log(Response);
+    .then(response => response.json())
+    .then(dati => {
+        console.log(dati)
     })
     .catch( error => {
         console.log(error);
     })
 }
 
+async function getAereoporti2(){
+
+    try
+    {
+        const risposta = await fetch('./data/aereoporti.js');
+        const dati = await risposta.json();
+        console.log(dati);
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+function createTable(dai){
+
+}
+
+function createTableHeader(){
+
+}
+
+function createTableRow(createTable){
+
+}
+
+
 getAereoport();
+console.log('Terminato');
